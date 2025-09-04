b810d28629ec7c496190f2abd9d5b7838e064f81
SUNNY_KIM <sam11222@naver.com>
2025-09-04 23:16:49 +0900

    fix : category type 피드백에 맞게 보완

--- src/main/kotlin/zipbap/app/domain/category/cookingtype/CookingType.kt
+++ src/main/kotlin/zipbap/app/domain/category/cookingtype/CookingType.kt
@@ -6,14 +6,11 @@ import zipbap.api.domain.base.BaseEntity
 @Entity
 @Table(name = "category_cooking_type")
 class CookingType(
+        @Column(name = "type", nullable = false, length = 50, unique = true)
+        val type: String,
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long? = null,
+        val id: Long? = null
 
-        @Column(name = "type", nullable = false,
-                length = 50, unique = true)
-        val type: String
-
-) : BaseEntity() {
-}
+) : BaseEntity()
